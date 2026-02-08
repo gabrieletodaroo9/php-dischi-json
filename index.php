5d17e83ce6e95532039802c1580bbac518d2924b
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2026-02-08 17:49:53 +0100

    create basic header main and footer with the data get by the json file

--- index.php
+++ index.php
@@ -12,27 +12,43 @@
 
 <body>
 
-    <header style="height: 80px;" class="bg-success d-flex justify-content-start align-items-center px-4">
-        <img src="./assets/logo-boolify.png" alt="logo" style="height: 45px;">
+    <header style="height: 90px;" class="bg-success d-flex justify-content-start align-items-center px-4">
+        <img src="./assets/logo-boolify.png" alt="logo" style="height: 50px;">
     </header>
 
-    <main class="bg-dark vh-100">
-        <h1 class="fs-2 text-center text-white text-uppercase py-4">Our disk</h1>
-        
-        <?php foreach ($disks as $disk){ ?>
-        
-        <p class="text-white">ciao <?php echo $disk["titolo"]?></p>
+    <main class="bg-dark h-100 py-4">
+        <h1 class="fs-2 text-center text-white text-uppercase py-4 fw-bold">Our disk</h1>
+        <div class="container">
+            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+                <?php foreach ($disks as $disk) { ?>
+                    <div class="col d-flex mb-5">
+                        <div class="card h-100 text-center bg-secondary text-white w-100 rounded-2 m-3">
+                            <img src="<?php echo $disk["cover"] ?>" class="card-img-top" alt="<?php echo $disk["titolo"] ?>">
 
-        <?php
-        }
-        ?> 
+                            <div class="card-body d-flex flex-column">
+                                <div class="d-flex justify-content-between align-items-center">
 
-
+                                    <h5 class="card-text small fw-bold"><?php echo $disk["titolo"] ?></h5>
+                                    <p class="card-text text-light small fw-bold"><?php echo $disk["artista"] ?></p>
+                                </div>
+                                <div class="mt-auto d-flex justify-content-between align-items-center">
+                                    <p class="fw-bold mb-1">Year:<?php echo $disk["anno"] ?></p>
+                                    <p class="fw-bold mb-0"><?php echo $disk["genere"] ?></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
 
     </main>
 
+    <footer class="bg-success">
 
-    <footer>
+    <div class="container d-flex justify-content-center align-items-center">
+        <span class="py-2"> © Boolify Tutti i diritti riservati.</span>
+    </div>
 
     </footer>
 
