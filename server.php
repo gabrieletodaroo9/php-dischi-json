226c7b8269191df9dc0f528af4728d9b92a9ee9b
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2026-02-08 18:40:41 +0100

    Transition from array to json object completed and file updated. Test all and add new disk. Exercise completed.

--- server.php
+++ server.php
@@ -14,4 +14,10 @@ $newDisk = [
     "genere" => $genere
 ];
 
-// var_dump($newDisk);
+
+$disks_text= file_get_contents("disks.json");
+$disks= json_decode($disks_text);
+$disks[]= $newDisk;
+$disk_text= json_encode($disks);
+file_put_contents("./disks.json",$disk_text);
+header("Location: index.php");
